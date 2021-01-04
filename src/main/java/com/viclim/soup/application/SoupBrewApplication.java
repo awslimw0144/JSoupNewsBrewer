88674a6b4f1c18dafef5e37135d9e66dff60a51b
awslimw0144 <awslimw0144@gmail.com>
2021-01-04 09:30:17 +0800

    Sedcond push

---
+++ src/main/java/com/viclim/soup/application/SoupBrewApplication.java
@@ -0,0 +1,7 @@
+package com.viclim.soup.application;
+
+public class SoupBrewApplication {
+    public static void main(String...args){
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/com.viclim.souptest/sanity/Test001_JunitIsWorking.java
@@ -0,0 +1,37 @@
+package com.viclim.souptest.sanity;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Basic Sanity Test")
+public class Test001_JunitIsWorking {
+    @Test
+    @DisplayName("Test 001 - Junit Is Working")
+    public void test001(){
+        assertEquals(0,0);
+    }
+
+    @Test
+    @DisplayName("Test 002 - JSoup Title Is Working")
+    public void test002(){
+        String html = "<html><head><title>First parse</title></head>"
+                + "<body><p>Parsed HTML into a doc.</p></body></html>";
+        Document doc = Jsoup.parse(html);
+        assertEquals("First parse",doc.title());
+    }
+
+    @Test
+    @DisplayName("Test 003 - JSoup Body Is Working")
+    public void test003(){
+        String html = "<html><head><title>First parse</title></head>"
+                + "<body><p>Parsed HTML into a doc.</p><p>Parsing again</p></body></html>";
+        Document doc = Jsoup.parseBodyFragment(html);
+        assertEquals("Parsed HTML into a doc.",doc.select("p").get(0).text());
+        assertEquals("Parsing again",doc.select("p").get(1).text());
+    }
+}
+
